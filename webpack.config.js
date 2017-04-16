a0f60adfe0f7f237ca1aaa66c421a2850a032e49
cvglass <christinevglass@gmail.com>
2017-04-15 19:21:37 -0500

    set up phaser game

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const webpack = require('webpack');
 const path = require('path');
 
+var phaserModule = path.join(__dirname, '/node_modules/phaser/');
+var phaser = path.join(phaserModule, 'build/custom/phaser-split.js'),
+  pixi = path.join(phaserModule, 'build/custom/pixi.js'),
+  p2 = path.join(phaserModule, 'build/custom/p2.js');
+
 const config = {
   entry: './app/index.jsx',
   output: {
@@ -8,14 +13,21 @@ const config = {
     filename: './public/bundle.js'
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.json', '*']
+    extensions: ['.js', '.jsx', '.json', '*'],
+    alias: {
+            'phaser': phaser,
+            'pixi.js': pixi,
+            'p2': p2,
+    },
   },
   module: {
     rules: [{
       test: /jsx?$/,
       exclude: /(node_modules|bower_components)/,
       use: [{
-        loader: 'babel-loader',
+        loaders: [
+            { test: /pixi.js/, loader: "script" }, 'babel-loader'
+        ],
         options: {
           presets: ['react', 'es2015', 'stage-2']
         }
